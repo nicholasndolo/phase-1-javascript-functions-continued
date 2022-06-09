33cdefa33fac25347b91b44a7cd5f37ce24c9b93
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-09 14:50:32 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(hobby="roller-skate"){
+    return`This Saturday, I want to ${hobby}!`
+}
+saturdayFun()
+
+const mondayWork = function(plan = "go to the office"){
+    return `This Monday, I will ${plan}.`
+}
+mondayWork()
+ 
+function wrapAdjective(msg = "*"){
+    return function innerFunction(message = "special"){
+        return `You are ${msg}${message}${msg}!`
+    }
+
+    let result = wrapAdjective('*')
+    let emphatic = result("a hard worker")
+
+}
